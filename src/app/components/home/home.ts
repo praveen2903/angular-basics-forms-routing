84b2b8157af50c5671b9b975f7b931a8cb36c863
praveen2903 <vvit20bq1a05l0@gmail.com>
2026-07-12 15:23:19 +0530

    put delte are done

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -37,7 +37,155 @@ export class Home {
     this.router.navigate(['/book', location.id, location.name]);
   }  
 
+homeTheory = `Then why does ChangeDetectorRef exist?
 
+It allows you to manually control Angular's change detection when Angular cannot detect changes by itself or when you want to optimize performance.
+
+1. markForCheck()
+
+Used with:
+
+changeDetection: ChangeDetectionStrategy.OnPush
+
+Example:
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+When using OnPush, Angular checks the component only when:
+
+An @Input() changes
+An event occurs
+An observable emits through the async pipe
+You call
+this.cdr.markForCheck();
+
+Without it, the UI may not update.
+
+Example:
+
+this.locations = response;
+this.cdr.markForCheck();
+
+This tells Angular:
+
+"Please check this component in the next change detection cycle."
+
+2. detectChanges()
+
+Runs change detection immediately.
+
+this.data = response;
+this.cdr.detectChanges();
+
+Angular instantly refreshes the component.
+
+Useful when changes occur outside Angular's zone.
+
+Example:
+
+setTimeout(() => {
+    this.name = "Praveen";
+    this.cdr.detectChanges();
+}, 1000);
+3. detach()
+
+Stops Angular from checking the component.
+
+constructor(private cdr: ChangeDetectorRef) {
+    this.cdr.detach();
+}
+
+Now Angular ignores this component completely.
+
+Useful for huge tables or dashboards.
+
+4. reattach()
+
+Enables change detection again.
+
+this.cdr.reattach();
+Why wasn't it required in your HttpClient example?
+
+HttpClient runs inside Angular's zone.
+
+this.hs.getLocations().subscribe(res => {
+    this.locations = res;
+});
+
+When the HTTP response arrives:
+
+Response received.
+Angular knows it happened.
+Angular automatically runs change detection.
+Template updates.
+
+No manual call needed.
+
+When is it actually needed?
+
+
+Case 1: Using OnPush -- asyncronous data addition to our local variables
+@Component({
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+Example: axios.get() asynchronounsly data added 
+this.locations = response;
+this.cdr.markForCheck();
+
+
+Case 2: Third-party libraries
+
+Example:
+new SomeLibrary().onData(data => {
+    this.locations = data;
+    this.cdr.detectChanges();
+});
+Angular doesn't know the callback happened.
+
+Case 3: Native DOM events-- handles DOM changes events like timeouts, intervals, eventlisteners, errorMessages
+document.addEventListener('click', () => {
+    this.count++;
+    this.cdr.detectChanges();
+});
+
+
+
+Case 4: NgZone.runOutsideAngular()
+this.zone.runOutsideAngular(() => {
+    setInterval(() => {
+        this.count++;
+        this.cdr.detectChanges();
+    }, 1000);
+});
+
+
+Case 5: detach(): this.cdr.detach();
+this.value = 100;   // UI won't update
+
+
+this.cdr.detectChanges();
+Why did many people use it for ExpressionChangedAfterItHasBeenCheckedError?
+
+Older code examples often added but for new ones need to add this:
+this.cdr.detectChanges();
+
+
+to suppress errors like:
+
+ExpressionChangedAfterItHasBeenCheckedError
+This is usually not the correct fix. That error typically indicates that a bound value changed after Angular had already checked the component. The better solution is to adjust the timing of the update (for example, moving logic to the appropriate lifecycle hook or restructuring the code), rather than forcing change detection.
+
+Summary
+Method	                                    Purpose	                                Common Use
+markForCheck()          Marks an OnPush component to be checked later	              OnPush strategy
+detectChanges()          Runs change detection immediately	                      Third-party callbacks, DOM APIs, code outside Angular
+detach()                Stops automatic change detection	                      Performance optimization
+reattach()              Restores automatic change detection	                      After detach()`
   homeTs= `
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { HomeService } from './home.service';
@@ -225,6 +373,10 @@ data = [
     index: 'homeService',
     value: this.homeService,
   },
+  {
+    index: 'theory',
+    value: this.homeTheory,
+  }
 ];
 
 setIndex = 'ts'
